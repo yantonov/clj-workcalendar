9c236cbba89980bc986c1717bb00eac6f665b626
Yury Antonov <yantonov@yandex.ru>
2015-03-11 01:19:26 +0500

    remove duplication

--- test/cljs_workcalendar/spider/consultantru_test.clj
+++ test/cljs_workcalendar/spider/consultantru_test.clj
@@ -12,26 +12,26 @@
 
 (deftest calendar-2015
   (let [items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
-    (are [y m d type]
+    (are [m d type]
       (= 1
          (count (filter #(and
-                          (= (:year %) y)
+                          (= (:year %) 2015)
                           (= (:month %) m)
                           (= (:day %) d)
                           (= (:type %) type)
                           )
                         items)))
-      2015 1 1  :holiday
-      2015 1 2  :holiday
-      2015 1 5  :holiday
-      2015 1 6  :holiday
-      2015 1 7  :holiday
-      2015 1 8  :holiday
-      2015 1 9  :holiday
-      2015 2 23 :holiday
-      2015 3 9  :holiday
-      2015 5 1  :holiday
-      2015 5 4  :holiday
-      2015 5 11 :holiday
-      2015 6 12 :holiday
-      2015 11 4 :holiday)))
+      1 1  :holiday
+      1 2  :holiday
+      1 5  :holiday
+      1 6  :holiday
+      1 7  :holiday
+      1 8  :holiday
+      1 9  :holiday
+      2 23 :holiday
+      3 9  :holiday
+      5 1  :holiday
+      5 4  :holiday
+      5 11 :holiday
+      6 12 :holiday
+      11 4 :holiday)))
