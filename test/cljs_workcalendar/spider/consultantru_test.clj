c109fb5b82d6bc48f0979b76ac1176adef629f3b
Yury Antonov <yantonov@yandex.ru>
2015-03-11 21:03:28 +0500

    review test

--- test/cljs_workcalendar/spider/consultantru_test.clj
+++ test/cljs_workcalendar/spider/consultantru_test.clj
@@ -6,130 +6,89 @@
   (:require [cljs-workcalendar.spider.workcalendarsource :as source])
   (:require [clojure.test :refer :all]))
 
-(deftest calendar-2015-special-day-count
-  (is (= 14 (count (filter #(= 2015 (:year %))
-                           (source/get-work-calendar (ConsultantRuWorkCalendarSource.)))))))
+(defn- special-days-for-year [year]
+  (let [all-items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
+    (sort-by (fn [x] (+ (* 31 (get x 0))
+                        (get x 1)))
+             (map #(vector (:month %)
+                           (:day %)
+                           (:type %))
+                  (filter #(= (:year %) year) all-items)))))
 
 (deftest calendar-2015
-  (let [items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
-    (are [m d type]
-      (= 1
-         (count (filter #(and
-                          (= (:year %) 2015)
-                          (= (:month %) m)
-                          (= (:day %) d)
-                          (= (:type %) type)
-                          )
-                        items)))
-      1 1  :holiday
-      1 2  :holiday
-      1 5  :holiday
-      1 6  :holiday
-      1 7  :holiday
-      1 8  :holiday
-      1 9  :holiday
-      2 23 :holiday
-      3 9  :holiday
-      5 1  :holiday
-      5 4  :holiday
-      5 11 :holiday
-      6 12 :holiday
-      11 4 :holiday)))
-
-(deftest calendar-2014-special-day-count
-  (is (= 14 (count (filter #(= 2014 (:year %))
-                           (source/get-work-calendar (ConsultantRuWorkCalendarSource.)))))))
+  (is (= [[1 1  :holiday]
+          [1 2  :holiday]
+          [1 5  :holiday]
+          [1 6  :holiday]
+          [1 7  :holiday]
+          [1 8  :holiday]
+          [1 9  :holiday]
+          [2 23 :holiday]
+          [3 9  :holiday]
+          [5 1  :holiday]
+          [5 4  :holiday]
+          [5 11 :holiday]
+          [6 12 :holiday]
+          [11 4 :holiday]]
+         (special-days-for-year 2015))))
 
 (deftest calendar-2014
-  (let [items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
-    (are [m d type]
-      (= 1
-         (count (filter #(and
-                          (= (:year %) 2014)
-                          (= (:month %) m)
-                          (= (:day %) d)
-                          (= (:type %) type)
-                          )
-                        items)))
-      1 1  :holiday
-      1 2  :holiday
-      1 3  :holiday
-      1 6  :holiday
-      1 7  :holiday
-      1 8  :holiday
-      3 10  :holiday
-      5 1  :holiday
-      5 2  :holiday
-      5 9  :holiday
-      6 12  :holiday
-      6 13  :holiday
-      11 3  :holiday
-      11 4  :holiday)))
-
-(deftest calendar-2013-special-day-count
-  (is (= 14 (count (filter #(= 2013 (:year %))
-                           (source/get-work-calendar (ConsultantRuWorkCalendarSource.)))))))
+  (is (=[ [1 1  :holiday]
+          [1 2  :holiday]
+          [1 3  :holiday]
+          [1 6  :holiday]
+          [1 7  :holiday]
+          [1 8  :holiday]
+          [3 10 :holiday]
+          [5 1  :holiday]
+          [5 2  :holiday]
+          [5 9  :holiday]
+          [6 12 :holiday]
+          [6 13 :holiday]
+          [11 3 :holiday]
+          [11 4 :holiday]]
+        (special-days-for-year 2014))))
 
 (deftest calendar-2013
-  (let [items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
-    (are [m d type]
-      (= 1
-         (count (filter #(and
-                          (= (:year %) 2013)
-                          (= (:month %) m)
-                          (= (:day %) d)
-                          (= (:type %) type)
-                          )
-                        items)))
-      1 1  :holiday
-      1 2  :holiday
-      1 3  :holiday
-      1 4  :holiday
-      1 7  :holiday
-      1 8  :holiday
-      3 8  :holiday
-      5 1  :holiday
-      5 2  :holiday
-      5 3  :holiday
-      5 9  :holiday
-      5 10  :holiday
-      6 12  :holiday
-      11 4  :holiday)))
-
-(deftest calendar-2012-special-day-count
-  (is (= 24 (count (filter #(= 2012 (:year %))
-                           (source/get-work-calendar (ConsultantRuWorkCalendarSource.)))))))
+  (is (=[[1 1  :holiday]
+         [1 2  :holiday]
+         [1 3  :holiday]
+         [1 4  :holiday]
+         [1 7  :holiday]
+         [1 8  :holiday]
+         [3 8  :holiday]
+         [5 1  :holiday]
+         [5 2  :holiday]
+         [5 3  :holiday]
+         [5 9  :holiday]
+         [5 10  :holiday]
+         [6 12  :holiday]
+         [11 4  :holiday]]
+        (special-days-for-year 2013))))
 
 (deftest calendar-2012
-  (let [items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
-    (are [m d type]
-      (= 1
-         (count (filter #(and
-                          (= (:year %) 2012)
-                          (= (:month %) m)
-                          (= (:day %) d)
-                          (= (:type %) type)
-                          )
-                        items)))
-      1 2   :holiday
-      1 3   :holiday
-      1 4   :holiday
-      1 5   :holiday
-      1 6   :holiday
-      2 23  :holiday
-      3 8   :holiday
-      3 9   :holiday
-      3 11  :workday
-      4 28  :workday
-      4 30  :holiday
-      5 1   :holiday
-      5 7   :holiday
-      5 8   :holiday
-      5 9   :holiday
-      5 12  :workday
-      6 9   :workday
-      6 11  :holiday
-      6 12  :holiday
-      11 5  :holiday
-      12 29 :workday
-      12 31 :holiday)))
+  (is (= [[1 2   :holiday]
+          [1 3   :holiday]
+          [1 4   :holiday]
+          [1 5   :holiday]
+          [1 6   :holiday]
+          [1 9   :holiday]
+          [2 23  :holiday]
+          [3 8   :holiday]
+          [3 9   :holiday]
+          [3 11  :workday]
+          [4 28  :workday]
+          [4 30  :holiday]
+          [5 1   :holiday]
+          [5 5   :workday]
+          [5 7   :holiday]
+          [5 8   :holiday]
+          [5 9   :holiday]
+          [5 12  :workday]
+          [6 9   :workday]
+          [6 11  :holiday]
+          [6 12  :holiday]
+          [11 5  :holiday]
+          [12 29 :workday]
+          [12 31 :holiday]]
+         (special-days-for-year 2012))))
