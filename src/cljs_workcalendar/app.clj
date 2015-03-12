37ca1f398928080bd6cd3657ecb0e26388528fcf
Yury Antonov <yantonov@yandex.ru>
2015-03-12 23:56:22 +0500

    formatter only formats data not output it to stream

--- src/cljs_workcalendar/app.clj
+++ src/cljs_workcalendar/app.clj
@@ -4,13 +4,19 @@
   (:import  [cljs_workcalendar.datasource.consultantru ConsultantRuWorkCalendarSource])
   (:require [cljs-workcalendar.format.formatinterface :as fmt])
   (:require [cljs-workcalendar.format.plaintextformatter])
-  (:import [cljs_workcalendar.format.plaintextformatter PlainTextWorkCalendarFormatter]))
+  (:import [cljs_workcalendar.format.plaintextformatter PlainTextWorkCalendarFormatter])
+  (:require [clojure.java.io :as io]))
 
 (defn main [args]
   (let [source (ConsultantRuWorkCalendarSource.)
         fmt (PlainTextWorkCalendarFormatter.)]
-    (fmt/format-work-calendar fmt (sort-by (fn [item] (format "%04d.%02d.%02d"
-                                                              (:year item)
-                                                              (:month item)
-                                                              (:day item)))
-                                           (source/get-work-calendar source)))))
+    (with-open [writer (io/writer "work-calendar.txt")]
+      (doseq [line (fmt/format-work-calendar
+                    fmt
+                    (sort-by (fn [item]
+                               (format "%04d.%02d.%02d"
+                                       (:year item)
+                                       (:month item)
+                                       (:day item)))
+                             (source/get-work-calendar source)))]
+        (.write writer line)))))
--- src/cljs_workcalendar/format/plaintextformatter.clj
+++ src/cljs_workcalendar/format/plaintextformatter.clj
@@ -5,11 +5,11 @@
 (defrecord PlainTextWorkCalendarFormatter []
   f/ICanFormatWorkCalendar
   (format-work-calendar [this work-calendar]
-    (with-open [writer (io/writer "test.txt")]
-      (doseq [item work-calendar]
-        (.write writer (format "%04d.%02d.%02d:%d\n"
-                               (:year item)
-                               (:month item)
-                               (:day item)
-                               (let [t (:type item)]
-                                 (if (= t :holiday) 0 1))))))))
+    (map (fn [item]
+           (format "%04d.%02d.%02d:%d\n"
+                   (:year item)
+                   (:month item)
+                   (:day item)
+                   (let [t (:type item)]
+                     (if (= t :holiday) 0 1))))
+         work-calendar)))
