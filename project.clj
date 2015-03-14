91f80541b5efcce51d1e3e7d1b7055c37f38267f
Yury Antonov <yantonov@yandex.ru>
2015-03-14 13:37:57 +0500

    enable cljs

--- project.clj
+++ project.clj
@@ -5,5 +5,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]
-                 [enlive "1.1.5"]]
-  :main cljs-workcalendar.app)
+                 [enlive "1.1.5"]
+                 [org.clojure/clojurescript "0.0-3058"]]
+  :main cljs-workcalendar.app
+  :plugins [[lein-cljsbuild "1.0.5"]]
+  :cljsbuild {
+              :builds [{
+                        ;; The path to the top-level ClojureScript source directory:
+                        :source-paths ["src-cljs"]
+                        ;; The standard ClojureScript compiler options:
+                        ;; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                                   :output-to "deploy/javascripts/main.js"
+                                   ;; default: target/cljsbuild-main.js
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
---
+++ src-cljs/hello.cljs
@@ -0,0 +1,5 @@
+(ns src-cljs.hello.cljs)
+
+(enable-console-print!)
+
+(println "Hello world!")
