1d24f1a5b1b22d70eef823da7b35995c5ea9c29e
Yury Antonov <yantonov@yandex.ru>
2015-03-18 23:57:13 +0500

    review is-workday tests

--- test-cljs/cljs_workcalendar/api_test.cljs
+++ test-cljs/cljs_workcalendar/api_test.cljs
@@ -10,11 +10,17 @@
 
     (are [year month day workday?]
       (= workday? (.is-workday service (js/Date. year (dec month) day)))
-      2012 1 1 false
-      2012 1 2 false
-      2012 1 3 false
-      2012 1 10 true
-      2012 1 13 true
-      2012 2 23 false
-      2013 3 11 true
-      2012 6 9 true)))
+      2012 1 1 false ; holiday
+      2012 1 2 false ; holiday
+      2012 1 3 false ; holiday
+      2012 1 10 true ; workday
+      2012 1 13 true ; workday
+      2012 2 23 false ; holiday
+      2012 3 8 false ; holiday
+      2012 3 9 false ; holiday
+      2012 3 10 false ; weekend
+      2012 3 11 true ; special workday
+      2012 6 9 true ; special workday
+      2012 6 16 false ; weekend
+      2012 6 17 false ;weekend
+      )))
