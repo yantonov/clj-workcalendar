9d75d0cc75eae1013eb2ff4b2c331078e7e22dcf
Yury Antonov <yantonov@yandex.ru>
2015-03-26 23:18:38 +0500

    parse work calendar from superjob dot ru

---
+++ src/cljs_workcalendar/datasource/superjobru.clj
@@ -0,0 +1,63 @@
+(ns cljs-workcalendar.datasource.superjobru
+  (:require [cljs-workcalendar.workdayitem])
+  (:import  [cljs_workcalendar.workdayitem WorkDayItem])
+  (:require [cljs-workcalendar.datasource.workcalendarsource :as source])
+  (:require [net.cgrand.enlive-html :as html])
+  (:require [clj-http.client :as http]))
+
+(defn- prev-month-day? [html-element]
+  (= (:class (:attrs html-element)) "pk_other"))
+
+(defn- weekend? [html-element]
+  (let [c (:class (:attrs html-element))]
+    (and (not (nil? c))
+         (.contains c "pk_holiday"))))
+
+(defn- day [html-element]
+  (Integer/parseInt (first (:content html-element))))
+
+(defn- extract-non-working-days [calendar-element year month]
+  (filter #(not (nil? %))
+          (map (fn [day-element day-of-week]
+                 (cond
+                   (prev-month-day? day-element)
+                   nil
+
+                   (and (>= day-of-week 1)
+                        (<= day-of-week 5)
+                        (weekend? day-element))
+                   (WorkDayItem. year month (day day-element) :holiday)
+
+                   (and (>= day-of-week 6)
+                        (<= day-of-week 7)
+                        (not (weekend? day-element)))
+                   (WorkDayItem. year month (day day-element) :workday)
+
+                   true
+                   nil))
+               (html/select calendar-element [:.pk_cells :div])
+               (apply concat (repeatedly #(range 1 8))))))
+
+(defn- get-work-calendar-for-year [year url]
+  (let [rawPage (:body (http/get url))
+        page (html/html-resource (java.io.StringReader. rawPage))
+        month-calendars (html/select page [:.pk_container])]
+    (apply concat (map (fn [calendar month-index]
+                         (extract-non-working-days calendar year month-index))
+                       month-calendars
+                       (iterate inc 1)))))
+
+
+(defrecord SuperjobRuWorkCalendarSource [])
+
+(extend-protocol source/WorkCalendarSource
+  SuperjobRuWorkCalendarSource
+  (get-work-calendar [this]
+    (concat (get-work-calendar-for-year 2015
+                                        "http://www.superjob.ru/proizvodstvennyj_kalendar/")
+            (get-work-calendar-for-year 2014
+                                        "http://www.superjob.ru/proizvodstvennyj_kalendar/2014/")
+            (get-work-calendar-for-year 2013
+                                        "http://www.superjob.ru/proizvodstvennyj_kalendar/2013/")
+            (get-work-calendar-for-year 2012
+                                        "http://www.superjob.ru/proizvodstvennyj_kalendar/2012/"))))
---
+++ test/cljs_workcalendar/datasource/superjobru_test.clj
@@ -0,0 +1,95 @@
+(ns cljs-workcalendar.datasource.superjobru-test
+  (:require [cljs-workcalendar.workdayitem])
+  (:import  [cljs_workcalendar.workdayitem WorkDayItem])
+  (:require [cljs-workcalendar.datasource.superjobru :as impl])
+  (:import  [cljs_workcalendar.datasource.superjobru SuperjobRuWorkCalendarSource])
+  (:require [cljs-workcalendar.datasource.workcalendarsource :as source])
+  (:require [clojure.test :refer :all]))
+
+(defn- special-days-for-year [year]
+  (let [all-items (source/get-work-calendar (SuperjobRuWorkCalendarSource.))]
+    (sort-by (fn [x] (+ (* 31 (get x 0))
+                        (get x 1)))
+             (map #(vector (:month %)
+                           (:day %)
+                           (:type %))
+                  (filter #(= (:year %) year) all-items)))))
+
+(deftest calendar-2015
+  (is (= [[1 1  :holiday]
+          [1 2  :holiday]
+          [1 5  :holiday]
+          [1 6  :holiday]
+          [1 7  :holiday]
+          [1 8  :holiday]
+          [1 9  :holiday]
+          [2 23 :holiday]
+          [3 9  :holiday]
+          [5 1  :holiday]
+          [5 4  :holiday]
+          [5 11 :holiday]
+          [6 12 :holiday]
+          [11 4 :holiday]]
+         (special-days-for-year 2015))))
+
+(deftest calendar-2014
+  (is (=[ [1 1  :holiday]
+          [1 2  :holiday]
+          [1 3  :holiday]
+          [1 6  :holiday]
+          [1 7  :holiday]
+          [1 8  :holiday]
+          [3 10 :holiday]
+          [5 1  :holiday]
+          [5 2  :holiday]
+          [5 9  :holiday]
+          [6 12 :holiday]
+          [6 13 :holiday]
+          [11 3 :holiday]
+          [11 4 :holiday]]
+        (special-days-for-year 2014))))
+
+(deftest calendar-2013
+  (is (=[[1 1  :holiday]
+         [1 2  :holiday]
+         [1 3  :holiday]
+         [1 4  :holiday]
+         [1 7  :holiday]
+         [1 8  :holiday]
+         [3 8  :holiday]
+         [5 1  :holiday]
+         [5 2  :holiday]
+         [5 3  :holiday]
+         [5 9  :holiday]
+         [5 10  :holiday]
+         [6 12  :holiday]
+         [11 4  :holiday]]
+        (special-days-for-year 2013))))
+
+(deftest calendar-2012
+  (is (= [[1 2   :holiday]
+          [1 3   :holiday]
+          [1 4   :holiday]
+          [1 5   :holiday]
+          [1 6   :holiday]
+          [1 9   :holiday]
+          [2 23  :holiday]
+          [3 8   :holiday]
+          [3 9   :holiday]
+          [3 11  :workday]
+          [4 28  :workday]
+          [4 30  :holiday]
+          [5 1   :holiday]
+          [5 5   :workday]
+          [5 7   :holiday]
+          [5 8   :holiday]
+          [5 9   :holiday]
+          [5 12  :workday]
+          [6 9   :workday]
+          [6 11  :holiday]
+          [6 12  :holiday]
+          [11 5  :holiday]
+          [12 29 :workday]
+          [12 31 :holiday]]
+         (special-days-for-year 2012))))
+
