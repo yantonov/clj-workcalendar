d878b1d1f132459ed648fa1e114915f35c3b2966
Yury Antonov <yantonov@yandex.ru>
2015-04-04 13:34:13 +0500

    update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1"]
+                 [clj-http "1.1.0"]
                  [enlive "1.1.5"]
-                 [org.clojure/clojurescript "0.0-3123"]]
+                 [org.clojure/clojurescript "0.0-3169"]]
   :main cljs-workcalendar.app
   :plugins [[lein-cljsbuild "1.0.5"]]
   :cljsbuild {
