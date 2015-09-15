0794b69d1f0cfc4c9603fc2d93d3abd6e6350d2c
Yury Antonov <yantonov@yandex.ru>
2015-09-15 23:05:14 +0500

    bumped up cljscript version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
                  [enlive "1.1.6"]
-                 [org.clojure/clojurescript "1.7.107"]]
+                 [org.clojure/clojurescript "1.7.122"]]
   :main cljs-workcalendar.app
   :plugins [[lein-cljsbuild "1.0.5"]]
   :cljsbuild {
