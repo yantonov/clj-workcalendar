5c4656f04713539d8b8ba8c25dd27851e5182637
Yury Antonov <yantonov@yandex.ru>
2015-10-02 10:45:40 +0500

    remove test data duplication

--- test/cljs_workcalendar/datasource/consultantru_test.clj
+++ test/cljs_workcalendar/datasource/consultantru_test.clj
@@ -4,7 +4,8 @@
   (:require [cljs-workcalendar.datasource.consultantru :as impl])
   (:import  [cljs_workcalendar.datasource.consultantru ConsultantRuWorkCalendarSource])
   (:require [cljs-workcalendar.datasource.workcalendarsource :as source])
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all])
+  (:require [cljs-workcalendar.datasource.calendar-test-data :as test-data]))
 
 (defn- special-days-for-year [year]
   (let [all-items (source/get-work-calendar (ConsultantRuWorkCalendarSource.))]
@@ -16,98 +17,21 @@
                        (filter #(= (:year %) year) all-items))))))
 
 (deftest calendar-2016
-  (is (= [[1 1  :holiday]
-          [1 4  :holiday]
-          [1 5  :holiday]
-          [1 6  :holiday]
-          [1 7  :holiday]
-          [1 8  :holiday]
-          [2 20 :workday]
-          [2 22 :holiday]
-          [2 23 :holiday]
-          [3 7  :holiday]
-          [3 8  :holiday]
-          [5 2  :holiday]
-          [5 3  :holiday]
-          [5 9  :holiday]
-          [6 13 :holiday]
-          [11 4 :holiday]]
+  (is (= (test-data/calendar-2016)
          (special-days-for-year 2016))))
 
 (deftest calendar-2015
-  (is (= [[1 1  :holiday]
-          [1 2  :holiday]
-          [1 5  :holiday]
-          [1 6  :holiday]
-          [1 7  :holiday]
-          [1 8  :holiday]
-          [1 9  :holiday]
-          [2 23 :holiday]
-          [3 9  :holiday]
-          [5 1  :holiday]
-          [5 4  :holiday]
-          [5 11 :holiday]
-          [6 12 :holiday]
-          [11 4 :holiday]]
+  (is (= (test-data/calendar-2015)
          (special-days-for-year 2015))))
 
 (deftest calendar-2014
-  (is (=[ [1 1  :holiday]
-          [1 2  :holiday]
-          [1 3  :holiday]
-          [1 6  :holiday]
-          [1 7  :holiday]
-          [1 8  :holiday]
-          [3 10 :holiday]
-          [5 1  :holiday]
-          [5 2  :holiday]
-          [5 9  :holiday]
-          [6 12 :holiday]
-          [6 13 :holiday]
-          [11 3 :holiday]
-          [11 4 :holiday]]
-        (special-days-for-year 2014))))
+  (is (= (test-data/calendar-2014)
+         (special-days-for-year 2014))))
 
 (deftest calendar-2013
-  (is (=[[1 1  :holiday]
-         [1 2  :holiday]
-         [1 3  :holiday]
-         [1 4  :holiday]
-         [1 7  :holiday]
-         [1 8  :holiday]
-         [3 8  :holiday]
-         [5 1  :holiday]
-         [5 2  :holiday]
-         [5 3  :holiday]
-         [5 9  :holiday]
-         [5 10  :holiday]
-         [6 12  :holiday]
-         [11 4  :holiday]]
-        (special-days-for-year 2013))))
+  (is (= (test-data/calendar-2013)
+         (special-days-for-year 2013))))
 
 (deftest calendar-2012
-  (is (= [[1 2   :holiday]
-          [1 3   :holiday]
-          [1 4   :holiday]
-          [1 5   :holiday]
-          [1 6   :holiday]
-          [1 9   :holiday]
-          [2 23  :holiday]
-          [3 8   :holiday]
-          [3 9   :holiday]
-          [3 11  :workday]
-          [4 28  :workday]
-          [4 30  :holiday]
-          [5 1   :holiday]
-          [5 5   :workday]
-          [5 7   :holiday]
-          [5 8   :holiday]
-          [5 9   :holiday]
-          [5 12  :workday]
-          [6 9   :workday]
-          [6 11  :holiday]
-          [6 12  :holiday]
-          [11 5  :holiday]
-          [12 29 :workday]
-          [12 31 :holiday]]
+  (is (= (test-data/calendar-2012)
          (special-days-for-year 2012))))
