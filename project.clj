7dd4ea032634a4f50c179281183f66e3f2f79ccc
yantonov <yantonov@yandex.ru>
2017-05-03 17:14:26 +0500

    bump up clojure version

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[org.clojure/clojure "1.7.0"]
+  [[org.clojure/clojure "1.8.0"]
    [clj-http "3.5.0"]
    [enlive "1.1.6"]
-   [org.clojure/clojurescript "1.7.228"]]
+   [org.clojure/clojurescript "1.9.521"]]
 
   :main cljs-workcalendar.app
 
