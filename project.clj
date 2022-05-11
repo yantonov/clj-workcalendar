922cb5781b9d9722ac166ac7d8de05b6cc19f193
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2022-05-11 12:40:13 +0200

    upgrade artifacts

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[org.clojure/clojure "1.8.0"]
-   [clj-http "3.7.0"]
+  [[org.clojure/clojure "1.11.1"]
+   [clj-http "3.12.3"]
    [enlive "1.1.6"]
-   [org.clojure/clojurescript "1.9.946"]]
+   [org.clojure/clojurescript "1.11.4"]]
 
   :main clj-workcalendar.app
 
